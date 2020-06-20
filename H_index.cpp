e082f154ba8bd37043ee78ac14ba79c19f0e46a5
urvashi-code1255 <urvashianand0906@gmail.com>
2020-06-20 14:38:35 +0530

    18,20

---
+++ H_index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) 
+    {
+        int n=citations.size(); // size of array
+        int left=0,right=n-1;     //applying binary search
+        
+        while(left<=right)
+        {
+            int mid=(left+right)/2; 
+            
+            if(citations[mid]<n-mid) 
+                left=mid+1;
+            else 
+			    right=mid-1;
+        }
+        return n-left;
+    }
+};
