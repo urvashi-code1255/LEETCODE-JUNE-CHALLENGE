2b39cf0c2b29f1f80f58a1e6529a643d47c5bf8f
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-06-25 16:57:27 +0530

    rename(duplicate number)

--- Find_the_duplicate_number(efficient).cpp
+++ Find_the_duplicate_number(efficient).cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+    int findDuplicate(vector<int>& nums) 
+    {
         int slow = nums[0] , fast = nums[0];
         
        do{
